038c376e5304adeee5b0b5c8e7a51275c2193459
Ranjith Kumar <123442191+S-RANJITH1@users.noreply.github.com>
2024-03-28 19:31:52 +0530

    DAY 5 Task completed

--- script.js
+++ script.js
@@ -30,29 +30,42 @@ for (let i in persDetails) {
 for (let j of persDetails) {
   console.log(j.name, j.age, j.city);
 }
+//for each
+persDetails.forEach(person => {
+  console.log("Name:", person.name);
+  console.log("Age:", person.age);
+  console.log("City:", person.city);
+})
 
 // task 2 => Create ur own resume data in JSON formate
-let resume = {
-  name: "Ranjith",
-  age: "27",
-  email: "ranjithppk2@gmail.com",
-  phone: "1234567890",
-  address: [
-    {
+
+let resume1 = {
+  personal_info: {
+    name: "Ranjith",
+    age: 27,
+    email: "ranjithppk2@gmail.com",
+    phone: "1234567890",
+    address: {
       city: "Neyveli",
       pincode: "607805",
       state: "Tamilnadu",
     },
-  ],
+  },
   education: [
     {
-      SSLC: "68%",
-      HLC: "80%",
-      UG: "65%",
+      degree: "SSLC",
+      percentage: "68%",
+    },
+    {
+      degree: "HLC",
+      percentage: "80%",
+    },
+    {
+      degree: "UG",
+      percentage: "65%",
     },
   ],
   languages: ["Tamil", "English"],
-  skils: ["communication", "problem solving"],
+  skills: ["communication", "problem solving"],
 };
-
-console.log(resume)
+console.log(JSON.stringify(resume1, null, 2));
